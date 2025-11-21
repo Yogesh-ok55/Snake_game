225edfa72c9bcbbb36543c1373e0630ea7a2e847
Yogesh Choudhary <127961738+Yogesh-ok55@users.noreply.github.com>
2025-11-21 21:52:24 +0530

    Update index.js

--- SnakeGame/Game/js/index.js
+++ SnakeGame/Game/js/index.js
@@ -4,7 +4,7 @@ const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
 const musicSound = new Audio('music/music.mp3');
-let speed = 19;
+let speed = 10;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
@@ -179,5 +179,6 @@ window.addEventListener('keydown', e => {
             console.log(e.key);
     }
 });
+
 
 
