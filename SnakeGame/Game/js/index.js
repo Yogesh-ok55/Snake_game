b2c79d1fd06d98c503b69e21e75d06abbaced41b
Yogesh Choudhary <127961738+Yogesh-ok55@users.noreply.github.com>
2025-11-21 22:01:06 +0530

    Reduce game speed and refactor code structure

--- SnakeGame/Game/js/index.js
+++ SnakeGame/Game/js/index.js
@@ -4,7 +4,7 @@ const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
 const musicSound = new Audio('music/music.mp3');
-let speed = 10;
+let speed = 9;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
@@ -179,6 +179,7 @@ window.addEventListener('keydown', e => {
             console.log(e.key);
     }
 });
+
 
 
 
